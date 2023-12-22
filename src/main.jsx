5359e4883993b2c1e1da6b7b557ec4d4c0880bb6
CodeWithKrish12 <codewithkrish12@gmail.com>
2023-12-23 00:04:26 +0530

    handled 404 page

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Layout from "./Layout"
+import Error from '../src/Extra/Error.jsx';
 import { Feed, VideoDetail, ChannelDetail, SearchFeed} from "./pages/pages"
 import { Route,RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<Layout />}>
+    <Route path='/' element={<Layout />} errorElement={<Error />}>
       <Route path='' exact element={<Feed />} />
       <Route path='/video/:id' exact element={<VideoDetail />} />
       <Route path='/channel/:id' exact element={<ChannelDetail />} />
